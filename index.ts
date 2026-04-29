8a6060d8bdc3a6900a10a927820e56fd94a37a98
Agrima <qvistn111@gmail.com>
2026-04-29 16:48:18 +1000

    Add concurrently for parallel script execution and update index.ts to run server and client concurrently

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrently, { Command } from "concurrently";
+
+concurrently([
+  { 
+     name: "server", 
+     command: "bun run dev",
+     cwd: "packages/server",
+     prefixColor: "green",
+  },
+  {
+    name: "client",
+    command: "bun run dev",
+    cwd: "packages/client",
+    prefixColor: "blue",
+  },
+  ]);
